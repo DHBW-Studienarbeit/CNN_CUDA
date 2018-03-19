65033a215df648df4f0765e02d42be9234e7257b
Benjamin Riedle <riedle.benjamin@gmail.com>
2018-03-19 10:51:09 +0100

    Max Pooling completly implemented without parallelism

--- CUDA_CNN/src/cuda_kernels.h
+++ CUDA_CNN/src/cuda_kernels.h
@@ -16,22 +16,25 @@ void loadPicture(float* arrayPtr, float* picturePtr);
 void convolution(float* inputPtr, float* outputPtr, float* weightPtr,
 		int inputDim_x, int inputDim_y, int weightDim_x, int weightDim_y);
 void maxPooling(float* inputPtr, float* outputPtr, int x_receptive, int y_receptive,
-							int inputDim_x, int inputDim_y, int outputDim_x, int outputDim_y);
+		int inputDim_x, int inputDim_y, int outputDim_x, int outputDim_y,
+		int convDim_x, int convDim_y, int nextDim_x, int nextDim_y,
+		int nextReceptive_x, int nextReceptive_y, LAYER_TYPE nextLayerType);
 
 
 } /* end namespace cuda */
 
-//for(int i = 0; i < 24; i++)
+/* column major */
+//for(int i = 0; i < 24; i++) /* rows in original row-major picture */
+//{
+//	for(int j = 0; j < 24; j++) /* columns in original row-major picture */
 //	{
-//		for(int j = 0; j < 24; j++)
+//		for(int k = 0; k < 5; k++) /* convolutional kernel rows in original picture */
 //		{
-//			for(int k = 0; k < 5; k++)
+//			for(int l = 0; l < 5; l++) /* convolutional kernel columns in original picture */
 //			{
-//				for(int l = 0; l < 5; l++)
-//				{
-//					arrayPtr[(k*5+l)*576+i*24+j] = picturePtr[(i+k)*24+j+l];
-//				}
+//				arrayPtr[(k*5+l)*576+i*24+j] = picturePtr[(i+k)*24+j+l];
 //			}
 //		}
 //	}
+//}
 #endif /* CUDA_KERNELS_H_ */
